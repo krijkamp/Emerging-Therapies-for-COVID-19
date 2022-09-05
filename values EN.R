b73fa3e327883a7b1ff8a50aee71ceb3998a5b56
Eline Krijkamp <e.krijkamp@erasmusmc.nl>
2022-09-05 14:11:37 +0200

    updates

--- values EN.R
+++ values EN.R
@@ -5,7 +5,6 @@
 # Load the values in shiny app applications
 
 n_eff_size    <- 0.82             # effect size treatment
-<<<<<<< Updated upstream
 ci_eff_size   <- c(0.72, 0.94)    # confidence interval of the effect size 
 
 n_eff_size_vent <- 0.640
@@ -20,7 +19,6 @@ LOS_noTrt    <- 13            # Length of stay without the therapy
 
 r_discount   <- 3             # the discount rate
 n_iter       <- 100           # then number of PSA iteration 
-=======
 ci_eff_size   <- c(0.47, 1.13)    # confidence interval of the effect size 
 
 
@@ -33,7 +31,7 @@ LOS_noTrt    <- 12            # Length of stay without the therapy
 
 r_discount    <- 3             # the discount rate
 n_iter       <- 100            # then number of PSA iteration 
->>>>>>> Stashed changes
+
 
 n_wtp        <- 100000        # the willingness-to-pay in $/QALY
 n_H_future   <- 88000         # the future number of hospitalized patients 
@@ -45,7 +43,7 @@ n_age_max <- 120       # max. age for life time horizon analysis
 n_DpC     <- 73        # days per cycle
 n_DpY     <- 365       # days per year
 d_c       <- 0.03      # discount rate 
-<<<<<<< Updated upstream
+
 
 # utility values
 # Hospitalized
@@ -63,5 +61,3 @@ sd_u_R_IC <- 0.301
 
 
 
-=======
->>>>>>> Stashed changes
